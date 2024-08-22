e4c88ba65faf21e7df3fec19cffa327f1cf1a1d7
Neander Fontes <neanderffontes@gmail.com>
2024-08-22 22:58:54 +0100

    Create especific List of Doctor's

--- api/src/main/java/med/voll/api/medicos/dtos/DadosListMedicosDTO.java
+++ api/src/main/java/med/voll/api/medicos/dtos/DadosListMedicosDTO.java
@@ -1,2 +1,16 @@
-package med.voll.api.medicos.dtos;public record DadosListMedicosDTO() {
+package med.voll.api.medicos.dtos;
+
+import med.voll.api.medicos.enums.EspecialidadeMedico;
+import med.voll.api.medicos.models.MedicoModel;
+
+public record DadosListMedicosDTO(
+        Long id,
+        String nome,
+        String email,
+        String crm,
+        EspecialidadeMedico especialidadeMedicos) {
+
+    public DadosListMedicosDTO(MedicoModel medicoModel) {
+        this(medicoModel.getId(), medicoModel.getNome(), medicoModel.getEmail(), medicoModel.getCrm(), medicoModel.getEspecialidadeMedicos());
+    }
 }
