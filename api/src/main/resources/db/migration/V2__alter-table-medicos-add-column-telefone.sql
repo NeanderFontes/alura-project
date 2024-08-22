294487cabe4e0b61600b5ba8b3b737c0b599cad5
Neander Fontes <neanderffontes@gmail.com>
2024-08-22 23:06:24 +0100

    Create migrations and implements diferents type of column

--- api/src/main/resources/db/migration/V2__alter-table-medicos-add-column-telefone.sql
+++ api/src/main/resources/db/migration/V2__alter-table-medicos-add-column-telefone.sql
@@ -1,18 +1,1 @@
-create table medicos(
-
-    id bigint not null auto_increment,
-    nome varchar(100) not null,
-    email varchar(100) not null unique,
-    crm varchar(6) not null unique,
-    especialidade_medicos varchar(100) not null,
-    logradouro varchar(100) not null,
-    bairro varchar(100) not null,
-    cep varchar(9) not null,
-    complemento varchar(100),
-    numero varchar(20),
-    uf char(2) not null,
-    cidade varchar(100) not null,
-
-    primary key(id)
-
-);
+alter table medicos add telefone varchar(20) not null;
